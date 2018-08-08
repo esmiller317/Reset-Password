074028faeba9569cc1ae44adc168bc7f23c72cca
esmiller317 <42219372+esmiller317@users.noreply.github.com>
2018-08-08 14:33:08 -0500

    Add files via upload

---
+++ passwordreset.py
@@ -0,0 +1,49 @@
+import sys
+print ('Enter your old password')
+oldPassword = raw_input()
+entries = 0
+theEnd = False
+
+print ('Enter your newpassword')
+newPassword = raw_input ()
+entries += 1
+
+for i in range (2)
+
+sys.exit()
+
+
+
+
+
+
+while newPassword == oldPassword and theEnd == False:
+        if entries < 3:
+            print ('Your new password cannot be the same as your old password.')
+            print ('Please choose a new password. Entries: ')+ str(entries)
+            newPassword = raw_input()
+            entries += 1
+        elif entries == 3:
+            print ('You have one more chance. Please choose a new password. Entries: ') + str (entries)
+            newPassword = raw_input ()
+            entries += 1
+        elif entries == 4:
+            print ('You have exceeded the number of entries.')
+            theEnd = True
+            break
+       
+        
+
+if theEnd == True :
+        print ('Your password is still ') + oldPassword + ('.')
+        print ('TheEnd value: ') + str (theEnd)
+        
+        
+elif newPassword != oldPassword:
+    print ('Your new password is:') + newPassword
+
+
+    
+
+print ('Done')
+
